f794a807c00c32c9c6ecb759c1a047e0f6bc6bc0
Mihail <mihan1993@inbox.ru>
2026-07-03 15:27:04 +0300

    10.7. Подготовка стилей проекта

--- pizza-app/eslint.config.js
+++ pizza-app/eslint.config.js
@@ -1,22 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
     languageOptions: {
       globals: globals.browser,
     },
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowContantExport: true }],
+      semi: [
+        'error',
+        'always',
+        {
+          omiLastInOneLineBlock: false,
+        },
+      ],
+      'comma-dagle': ['error', ' never'],
+      quotes: ['error', 'single'],
+      'react/prop-types': [0],
+      indent: ['error', 'tab'],
+    },
   },
-])
+]);
