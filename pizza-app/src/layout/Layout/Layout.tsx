74385d84fa08c07f57c8f84c96214f73fcc934e5
Mikhail Vitebskiy <mihan1993@inbox.ru>
2026-07-13 00:30:55 +0300

    11.6. Упражнение

--- pizza-app/src/layout/Layout/Layout.tsx
+++ pizza-app/src/layout/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from 'react-router-dom';
+import styles from './Layout.module.css'
 
 export function Layout() {
   return (
     <>
-      <div>
-        <div>
+      <div className={styles['layout']} >
+        <div className={styles['menu']} >
           <Link to="/">Меню</Link>
           <Link to="/cart">Корзина</Link>
         </div>
--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+
 import { RouterProvider } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu.tsx';
 import { Cart } from './pages/Cart/Cart.tsx';
 import { Error } from './pages/Error/Error.tsx';
-import { Layout } from './layout/Menu/Menu.tsx';
+import { Layout } from './layout/Layout/Layout.tsx';
+
 
 const rounter = createBrowserRouter([
   {
