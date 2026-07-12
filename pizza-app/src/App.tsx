40e6e1cfb95f26896dc49d1c4e24c1cb38bccf04
Mikhail Vitebskiy <mihan1993@inbox.ru>
2026-07-12 11:19:16 +0300

    Создание роутов (старая весия)

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,25 +1,42 @@
-import {  useState, type MouseEvent } from 'react';
+import { useState, type MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
-
   const [counter, setCounter] = useState<number>(0);
 
-  const addCounter = (e: MouseEvent<HTMLButtonElement>) => {
+  const addCounter = (e: MouseEvent) => {
     console.log(e);
-    console.log(counter);
-    setCounter(2)
-  }
+  };
+
+  
 
   return (
     <>
-      <Button onClick={addCounter} appearence='small' >Привет</Button>
-      <Button onClick={addCounter} appearence='big' >Привет</Button>
-      <Input placeholder='Email' />
-    </>
-  )
+      <Button onClick={addCounter} appearence="small">
+        Привет
+      </Button>
+      <Button onClick={addCounter} appearence="big">
+        Привет
+      </Button>
+      <Input placeholder="Email" />
 
+      <div className="">
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/*" element={<Error/>} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
